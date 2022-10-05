f43cc0dd055a374be6728f60ca20a8d4f4b957e4
katredford <katharine.redford@gmail.com>
2022-10-05 13:57:03 -0500

    add api fetch, start dropdown menu

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '67e533029bmshabe72dce0f8ac6fp13656cjsn8fdc977e8776',
+		'X-RapidAPI-Host': 'translated-mymemory---translation-memory.p.rapidapi.com'
+	}
+};
+
+const lang = "ga"
+
+// fetch('https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=en%7C' + lang + '&q=meow&mt=1&onlyprivate=0&de=a%40b.c', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+//   .catch(err => console.error(err));
+  
+fetch('https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=en%7C' + lang + '&q=meow&mt=1&onlyprivate=0&de=a%40b.c', options)
+
+
+  // Convert the response to JSON
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
+      console.log(data)
+  })
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
